2cfaea2e4585358b74f57843b4352db9a847ef23
shipramargurate <shivanikumari22322@gmail.com>
2022-08-29 21:11:17 +0530

    [SHIVANI] : added Ability to find maximum in given String

--- src/com/bridgelab/generics/FindMaximumMain.java
+++ src/com/bridgelab/generics/FindMaximumMain.java
@@ -4,8 +4,8 @@ public class FindMaximumMain {
 
 	public static void main(String[] args) {
 		FindMaximum maximum = new FindMaximum();
-		System.out.println("Maximumn in given integers is :");
-		maximum.checkMaximum(9.9f, 1.9f, 2.9f);
+		System.out.println("Maximumn in given Strings is :");
+		maximum.checkMaximum("abc", "lmn", "jkl");
 	}
 
 }
